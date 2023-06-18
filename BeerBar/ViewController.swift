e22d2d583dae705e304a4b809a15624443cce750
Никита Суровцев <nikitasurovcev@Nikitas-MacBook-Pro.local>
2023-06-18 20:56:57 +0300

    Add func

--- BeerBar/ViewController.swift
+++ BeerBar/ViewController.swift
@@ -8,12 +8,84 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var barName: UILabel!
+    
+    @IBOutlet weak var alivariya: UILabel!
+    @IBOutlet weak var baltika: UILabel!
+    @IBOutlet weak var korona: UILabel!
+    @IBOutlet weak var bud: UILabel!
+    @IBOutlet weak var tuborg: UILabel!
+    
 
+    
+    @IBOutlet var beerVolume: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        barName.text = "Dreenkeria"
+        
+        alivariya.text = "Аливария"
+        alivariya.font = UIFont.boldSystemFont(ofSize: 20)
+        baltika.text = "Балтика"
+        baltika.font = UIFont.boldSystemFont(ofSize: 20)
+        korona.text = "Корона"
+        korona.font = UIFont.boldSystemFont(ofSize: 20)
+        bud.text = "Bud"
+        bud.font = UIFont.boldSystemFont(ofSize: 20)
+        tuborg.text = "Tuborg"
+        tuborg.font = UIFont.boldSystemFont(ofSize: 20)
+
+//        for index in 0..<beerVolume.count {
+//            [index].titleLabel?.text = "\(index)"
+//        }
+        
     }
 
-
+    @IBAction func newAction(_ sender: Any) {
+        Swift.print("Новая смена")
+    }
+    
+    @IBAction func becomeZero(_ sender: Any) {
+        print("Обнулили")
+    }
+     
+    @IBAction func buyBeer(_ sender: UIButton) {
+        let tag = sender.tag
+        let row = tag / 10
+        let column = tag % 10
+        
+        var buttonName = ""
+        var beerNameforCollection = ""
+        
+        switch row  {
+        case 0: beerNameforCollection = alivariya.text!
+        case 1:
+            beerNameforCollection = baltika.text!
+        case 2:
+            beerNameforCollection = korona.text!
+        case 3:
+            beerNameforCollection = bud.text!
+        case 4:
+            beerNameforCollection = tuborg.text!
+        default:
+            break
+        }
+        
+        switch column  {
+        case 0:
+            buttonName = "0.33"
+        case 1:
+             buttonName = "0.5"
+        case 2:
+            buttonName = "1.0"
+        default:
+            break
+        }
+        
+        print("Было куплено пиво  \(beerNameforCollection) объёмом \(buttonName)л")
+    }
+    
 }
 
